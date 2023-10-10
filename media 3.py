75d153fd9a382622df931fb6eb17dfe18632a014
hiarleyCsilva <hiarley.silva2@aluno.ce.gov.br>
2023-10-10 16:50:16 -0300

    adicionado recurso de input

--- media 3.py
+++ media 3.py
@@ -1,5 +1,5 @@
-n1 = 7
-n2 = 7
-n3 = 10
+n1 = float ( input("digite a nota 1:"))
+n2 = float ( input("digite a nota 2:"))
+n3 = float ( input("digite a nota 3:"))
 media = (n1+n2+n3)/3
 print ("sua media é", media)
